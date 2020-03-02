7b34e5ba419d89d00db027a33a1a549e82e99177
RamimyKely <nyaina.mamitiana@gmail.com>
2020-03-02 17:44:50 +0300

    11.0

--- many2one_item_limit/models/res_config_settings.py
+++ many2one_item_limit/models/res_config_settings.py
@@ -6,4 +6,17 @@ from odoo import models, fields, api
 class ResConfigSettings(models.TransientModel):
     _inherit = 'res.config.settings'
 
-    many2one_limit_param = fields.Integer(string='Many2one item limit', config_parameter='many2one.limit')
+    many2one_limit_param = fields.Integer(string='Many2one item limit')
+
+    @api.model
+    def get_values(self):
+        res = super(ResConfigSettings, self).get_values()
+        res.update(
+            many2one_limit_param=int(self.env['ir.config_parameter'].sudo().get_param('many2one.limit')),
+        )
+        return res
+
+    @api.multi
+    def set_values(self):
+        super(ResConfigSettings, self).set_values()
+        self.env['ir.config_parameter'].sudo().set_param('many2one.limit', self.many2one_limit_param)
--- many2one_item_limit/static/src/js/relational_fields.js
+++ many2one_item_limit/static/src/js/relational_fields.js
@@ -7,14 +7,13 @@ odoo.define('many2one_item_limit.relational_fields', function (require) {
         init: function (parent, name, record, options) {
             var self = this;
             this._super.apply(this, arguments);
-            this._rpc({
+            self._rpc({
                 model: 'ir.config_parameter',
                 method: 'get_param',
-                args: [['many2one.limit']],
+                args: ['many2one.limit'],
             }).then(function (result) {
                 self.limit = parseInt(result);
             });
-
         }
     })
 });
